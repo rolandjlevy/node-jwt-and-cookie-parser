c3feb0feacb8d0b25264ee1a49db3b9d906f61a6
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-10-31 23:49:41 +0000

    Refactored

--- src/middleware/cookieJwtAuth.js
+++ src/middleware/cookieJwtAuth.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 
 exports.cookieJwtAuth = (req, res, next) => {
   const token = req.cookies.token;
-  console.log({ token })
   try {
     const user = jwt.verify(token, process.env.MY_SECRET);
     req.user = user;
